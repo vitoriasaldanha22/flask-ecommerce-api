661cbc018b5ed4359baa500c4fff4a6db1e04af8
Vitoria Saldanha <saldanhavitoria152@gmail.com>
2026-03-24 23:45:24 -0300

    Primeiro commit: Estrutura básica da API Flask

---
+++ app.py
@@ -0,0 +1,50 @@
+# importação 
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+# criei o banco: 
+# >>> db.create_all()
+# >>> db.session.commit()
+# >>> exit()
+
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.json
+    if 'name' in data and 'price' in data:
+        product = Product(name=data['name'], price=data['price'], description=data.get('description', ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"}), 201
+    
+    return jsonify({"message": "Invalid product data"}), 400        
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product deleted successfully"}), 200
+    
+    return jsonify({"message": "Product not found"}), 404
+
+# Definir uma rota raiz (pagina inicial) e a função que será executada ao ser requisitada 
+@app.route('/')
+def hello_word():
+    return 'Hello Word'
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
